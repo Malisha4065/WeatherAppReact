3283aaf552f247515cec7f5212dbcc7b210abe9a
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2023-08-15 22:05:43 +0530

    shows weather details for a given city

--- src/components/WeatherService.js
+++ src/components/WeatherService.js
@@ -8,69 +8,87 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import TextField from "@mui/material/TextField";
 import { Container } from "@mui/material";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const WeatherService = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [searchbox, setSearchbox] = useState("");
+
+  async function getWeather(location) {
+    const API_KEY = "6bd27019db3b4dc2a2a142218230808";
+
+    if (location === '') {
+      location = 'london';
+    }
+
+    const LOCATION_INPUT = location;
+
+    await axios
+      .get(
+        `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${LOCATION_INPUT}&aqi=no`
+      )
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        setWeatherData(res.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  }
+
+  // api request
+  useEffect(() => {
+    console.log(searchbox);
+    getWeather(searchbox);
+  }, [searchbox]);
+
+  const handleSearch = (e) => {
+    setSearchbox(e.target.value);
+  }
+
   return (
     <Container>
-      <div>WeatherService</div>
-      <BasicTable />
-    </Container>
-  );
-};
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
-function BasicTable() {
-  return (
-    <Container>
+      <div>Malisha's WeatherService</div>
+      <Container>
       <TextField
         id="standard-search"
         label="Search field"
         type="search"
-        variant="standard"
+        variant="filled"
+        onChange={handleSearch}
         sx={{ marginTop: 5 }}
       />
-      <TableContainer sx={{ marginTop: 5}} component={Paper}>
+      <TableContainer sx={{ marginTop: 5 }} component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell align="center">Location</TableCell>
+              <TableCell align="center">Humidity</TableCell>
+              <TableCell align="center">Pressure</TableCell>
+              <TableCell align="center">Wind Speed</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            
               <TableRow
-                key={row.name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+              
+                <TableCell align="center">{weatherData?.location.name}</TableCell>
+                <TableCell align="center">{weatherData?.current.humidity}</TableCell>
+                <TableCell align="center">{weatherData?.current.pressure_mb}</TableCell>
+                <TableCell align="center">{weatherData?.current.wind_kph}</TableCell>
               </TableRow>
-            ))}
           </TableBody>
         </Table>
       </TableContainer>
     </Container>
+    </Container>
   );
-}
+};
+
+
 
 export default WeatherService;
